a324493ffd2016c6ca12bd95fbf5342a17c79d3e
Furion <mgi_179@hotmail.com>
2014-01-13 21:18:41 +0800

    Good Idea

--- Assets/OtherScript.cs
+++ Assets/OtherScript.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class OtherScript : MonoBehaviour {
 
+	public GameObject whichObject;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		whichObject.transform.Translate(0,1*Time.deltaTime,0);
 	}
 }
